739b54cb6a95f7ada18e646c39db7f53cf19c663
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-05-20 15:22:01 +0200

    responsive iphone new menu hamburger

--- assets/footer.php
+++ assets/footer.php
@@ -1,52 +1,5 @@
     <!-- <section class="vertical-scrolling"> -->
-        <footer>
-        <div class="footer100">
-            <div class="mainFooter">
-                <div class="contentFooter">
-                    
-                    <div class="content2Footer">
-                    <img src="./img/INFINITY.png" class="logoFooter">
-
-                    <div class="contactFooter">
-                                <h5 class="titleFooter"> Contactez-nous </h5>
-                                <p> Rue de Wallonie 9b 4460 Grâce-Hollogne, Belgique </p>
-                                <p> info@infinity-agency.be </p> 
-                            </div>
-                        <div class="socialFooter">
-                            
-                            <div class="pictlogo">
-                                <h5 class="titleFooter">Suivez-nous sur les reseaux sociaux</h5>
-                                <div class="socialIcon">
-                                    <div class="soialIconLogo1"><a href="https://facebook.com"><i class="fab fa-facebook"></i></a></div>
-                                    <div class="soialIconLogo2"><a href="https://www.instagram.com/?hl=fr"><i class="fab fa-instagram-square"></i></a></div>
-                                    <div class="soialIconLogo3"><a href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a></div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- <div class="infoFooter">
-                            <h5 class="titleFooter">More</h5>
-                            <ul>
-                                <li><a href="">About Us</a></li>
-                                <li><a href="">Services</a></li>
-                                <li><a href="">Our Team</a></li>
-                                <li><a href="">Contact</a></li>
-                            </ul>
-                        </div> -->
-                            
-                        </div>
-                    </div>
-                    <div class="copyrightFooter">
-                        <p> Copyright 2021 © Infinity All rights reserved. </p> 
-                        <ul>
-                            <li>Terms of Service</li>
-                            <li> Privacy Policy </li>
-                            <li> Legal </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        
         </div>
     <!-- </section> -->
 </div>
