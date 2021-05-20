865c032bed4d38a79c0a4155d0f18a00d77dccb6
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-05-20 11:34:16 +0200

    new menu hamburger in progress

--- assets/menu4.php
+++ assets/menu4.php
@@ -2,23 +2,46 @@
 <div class="bg-cover" class="none"></div>
 
 <header>
-
 	<button class="hamburger-shell" aria-expanded="false">
 		<div class="hamb top"></div>
-			<div class="hamb middle"></div>
-		<div class="menu-name">MENU</div>
-	</button>
-
-	<nav id="menu">
-		<ul id="menu1">
-			<li data-menuanchor="firstPage"><a href="#firstPage">Home</a></li>
-			<li data-menuanchor="secondPage"><a href="#secondPage">Portfolio</a></li>
-			<li data-menuanchor="thirdPage"><a href="#3rdPage">About Us</a></li>
-			<li data-menuanchor="fourthPage"><a href="#4rdPage">Our staff</a></li>
-			<li data-menuanchor="fourthPage"><a href="#4rdPage">Contact</a></li>
-		</ul>
+		<div class="hamb middle"></div>
+        <div class="menu-name">MENU</div>
+    </button>
+	
+    <nav id="menu">
+		<img src="./img/infinity_one.png"/>
+        <ul id="menu1">
+            <li data-menuanchor="firstPage"><a href="#firstPage">Home</a></li>
+            <li data-menuanchor="thirdPage"><a href="#secondPage">About Us</a></li>
+            <li data-menuanchor="secondPage"><a href="#3rdPage">Partenaires</a></li>
+            <li data-menuanchor="fourthPage"><a href="#4rdPage">Influenceurs</a></li>
+            <li data-menuanchor="fifthPage"><a href="#5rdPage">Our Team</a></li>
+            <li data-menuanchor="sixPage"><a href="#6rdPage">Contact</a></li>
+        </ul>
+		<div class="socialIconMenu">
+			<a href="www.facebook.com"><i class="fab fa-facebook"></i></a>
+			<a href="www.instagram.com"><i class="fab fa-instagram-square"></i></a>
+			<a href="www.instagram.com"><i class="fab fa-linkedin"></i></a>
+		</div>
+		<div class="newsletterMenu">
+			<p class="stay">Stay Tuned</p>
+			<p class="news"> Newsletter </p>
+		</div>
+		<div class="buttonsMenu">
+			<a>Influenceurs</a>
+			<a>Partenaires</a>
+		</div>
+		<div class="infosMenu">
+			<p>Infinity Agency</p>
+			<p> Rue de Wallonie, 9</p>
+			<p> 4460 Grâce-Hollogne </p>
+			<p> Belgique </p>
+			<p> contact@infinity-agency.com </p>
+		</div>
+		<div class="copyrightMenu">
+			<a>Mention légales Copyright Infinity Agency </a>
+		</div>
 	</nav>
-</header>
-
+	</header>
 
 
